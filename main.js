fcc8e232e0891001dddc3cf734ae612304654f50
steffaneleal <steffane.leal13@gmail.com>
2023-10-20 12:54:29 -0300

    versão 6
    - ajuste no menu hambúrguer do header (falta deixar no modelo unect)
    - ajuste no footer (falta deixar as coisas no seus determinados lugares no responsivo)

---
+++ main.js
@@ -0,0 +1,10 @@
+
+/*MENU HAMBÚRGUER HEADER*/
+/*declaração de variáveis*/
+var menu = document.querySelector('nav ul');
+var menuBar = document.querySelector('nav .cabecalho-menu-hamburguer');
+var onMenu = document.querySelector('nav .cabecalho-menu-hamburguer img');
+
+menuBar.addEventListener('click', function(){
+    menu.classList.toggle('active'); /*faz com que o clique funcione*/
+})
